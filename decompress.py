3bace3523adc84e2e9bcadf262ba41d06c583bfe
oliverbaileysmith <smitholiverbailey@gmail.com>
2024-02-01 14:17:16 -0500

    Construct literal/length and distances trees using code length tree

--- decompress.py
+++ decompress.py
@@ -102,6 +102,30 @@ class HuffmanTree:
 		# Assign symbol at leaf node
 		curr.symbol = symbol
 
+# Construct canonical Huffman tree from alphabet and bit length list
+def huffman_tree_from_alphabet_and_bl_list(alphabet, bl_list):
+	max_bits = max(bl_list)
+
+	# Get number of codes for each bit length
+	bl_count = [0] * len(bl_list)
+	for i in range(max_bits + 1):
+		for bl in bl_list:
+			if bl == i and i != 0:
+				bl_count[i] += 1
+
+	# Compute smallest code for each bit length
+	next_code = [0, 0]
+	for bit_length in range(2, max_bits + 1):
+		next_code.append((next_code[bit_length - 1] + bl_count[bit_length - 1]) << 1)
+
+	# Construct canonical tree
+	tree = HuffmanTree()
+	for symbol, bit_length in zip(alphabet, bl_list):
+		if bit_length != 0:
+			tree.insert(next_code[bit_length], bit_length, symbol)
+			next_code[bit_length] += 1
+	return tree
+
 # Decode a symbol from streamer using tree
 def decode_symbol(streamer, tree):
 	curr = tree.root
@@ -143,29 +167,57 @@ def inflate_compressed_block(streamer, literal_length_tree, distance_tree, out_d
 			for i in range(length):
 				out_data.append(out[-distance])
 
-# Construct canonical Huffman tree from alphabet and bit length list
-def huffman_tree_from_alphabet_and_bl_list(alphabet, bl_list):
-	max_bits = max(bl_list)
+# Code length order table
+CodeLengthCodesOrder = [16, 17, 18, 0, 8, 7, 9, 6, 10, 5, 11, 4, 12, 3, 13, 2, 14, 1, 15]
 
-	# Get number of codes for each bit length
-	bl_count = [0] * len(bl_list)
-	for i in range(max_bits + 1):
-		for bl in bl_list:
-			if bl == i and i != 0:
-				bl_count[i] += 1
+# Create literal/length and distance trees by creating and using code length tree
+def decode_trees(streamer):
+	# Number of literal/length codes
+	HLIT = streamer.read_bits(5) + 257
 
-	# Compute smallest code for each bit length
-	next_code = [0, 0]
-	for bit_length in range(2, max_bits + 1):
-		next_code.append((next_code[bit_length - 1] + bl_count[bit_length - 1]) << 1)
+	# Number of distance codes
+	HDIST = streamer.read_bits(5) + 1
 
-	# Construct canonical tree
-	tree = HuffmanTree()
-	for symbol, bit_length in zip(alphabet, bl_list):
-		if bit_length != 0:
-			tree.insert(next_code[bit_length], bit_length, symbol)
-			next_code[bit_length] += 1
-	return tree
+	# Number of code length codes
+	HCLEN = streamer.read_bits(4) + 4
+
+	# Read code lengths for the code length alphabet
+	code_length_bl_list = [0] * 19
+	for i in range(HCLEN):
+		code_length_bl_list[CodeLengthCodesOrder[i]] = r.read_bits(3)
+
+	# Construct code length tree
+	code_length_tree = huffman_tree_from_alphabet_and_bl_list(range(19), code_length_bl_list)
+
+	# Read literal/length + distance code length list
+	bl_list = []
+	while len(bl_list) < HLIT + HDIST:
+		symbol = decode_symbol(r, code_length_tree)
+		assert(0 <= symbol <= 18), "Invalid symbol"
+
+		if symbol < 16: # literal value
+			bl_list.append(symbol)
+		elif symbol == 16:
+			# Copy previous code length 3 to 6 times
+			prev_code_length = bl_list[-1]
+			repeat_length = r.read_bits(2) + 3
+			for i in range(repeat_length):
+				bl_list.append(prev_code_length)
+		elif symbol == 17:
+			# Repeat code length 0 3 to 10 times
+			repeat_length = r.read_bits(3) + 3
+			for i in range(repeat_length):
+				bl_list.append(0)
+		else: # symbol == 18
+			# Repeat code length 0 11 to 138 times
+			repeat_length = r.read_bits(7) + 11
+			for i in range(repeat_length):
+				bl_list.append(0)
+
+	# Construct trees
+	literal_length_tree = huffman_tree_from_alphabet_and_bl_list(range(286), bl_list[:HLIT])
+	distance_tree = huffman_tree_from_alphabet_and_bl_list(range(30), bl_list[HLIT:])
+	return literal_length_tree, distance_tree
 
 # Inflate block where BTYPE == 0b01
 def inflate_block_fixed_huffman(streamer, out_data):
